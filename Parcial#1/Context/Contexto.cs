7a6045247adcdde4b2129bb7bddfd94c2232ba35
josueRusso <josuerusso1911@gmail.com>
2023-06-05 18:28:12 -0400

    Creando el Contexto

---
+++ Parcial#1/Context/Contexto.cs
@@ -0,0 +1,6 @@
+﻿namespace Parcial_1.Context
+{
+    public class Contexto
+    {
+    }
+}
--- Parcial#1/Model/Ingresos.cs
+++ Parcial#1/Model/Ingresos.cs
@@ -1,6 +1,20 @@
-﻿namespace Parcial_1.Model
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace Parcial_1.Model
 {
     public class Ingresos
     {
+        [Key]
+        public int IngresoId { get; set; }
+
+        [Required(ErrorMessage = "Debe Contener una Fecha")]
+        public String? Fecha { get; set; }
+
+        [Required(ErrorMessage = " Debe Contener un Concepto")]
+        public String? Concepto { get; set; }
+
+        [Required(ErrorMessage = " Debe Contener un Monto")]
+        public String? Monto { get; set; }
     }
 }
